60c1695c6bb4c575e43eefdf2a6248cb60237e93
johnnyL787 <124469687+johnnyL787@users.noreply.github.com>
2024-08-02 16:16:26 -0700

    highlight testing

---
+++ Assets/Scripts/HighlightOnMouseOver.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class HighlightOnMouseOver : MonoBehaviour
+{
+    private Color originalColor;
+    public Color highlightColor = Color.yellow;
+    private SpriteRenderer spriteRenderer;
+    private bool isHighlighted = false;
+
+    void Start()
+    {
+        // Get the SpriteRenderer component of the game object
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        // Save the original color of the game object
+        originalColor = spriteRenderer.color;
+    }
+
+    void Update()
+    {
+        // Create a ray from the camera to the mouse position
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+
+        // Check if the ray hits any collider
+        if (hit.collider != null)
+        {
+            // Check if the game object hit by the ray is this game object
+            if (hit.collider.gameObject == gameObject)
+            {
+                if (!isHighlighted)
+                {
+                    // Change the color of the game object to the highlight color
+                    spriteRenderer.color = highlightColor;
+                    isHighlighted = true;
+                }
+            }
+            else
+            {
+                if (isHighlighted)
+                {
+                    // Revert the color of the game object to the original color
+                    spriteRenderer.color = originalColor;
+                    isHighlighted = false;
+                }
+            }
+        }
+        else
+        {
+            if (isHighlighted)
+            {
+                // Revert the color of the game object to the original color
+                spriteRenderer.color = originalColor;
+                isHighlighted = false;
+            }
+        }
+    }
+}
